2efb6d0a0f4cb21f508da174390bbbb88436a6b6
karneds <karneds@gmail.com>
2012-05-27 11:41:46 +0400

    fixed tests

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -5,7 +5,7 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/config.php');
         parent::setUp();
     }
 
